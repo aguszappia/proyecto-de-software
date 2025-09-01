b720d3ae4b99b41bd4634e6574de119733bcc8d4
Nicolás Ivan Basaj Laborevich <nicobas2004@gmail.com>
2025-09-01 17:44:23 -0300

    agrego rama de multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,8 @@
 
 def calcular (num1, num2, operacion):
-    if operacion == '-':
+    if (operacion == '*'):
+        return multiplicacion(num1,num2)
+    elif operacion == '-':
         return resta(num1, num2)
     elif operacion == '+':
         return sumar(num1, num2)
@@ -12,3 +14,5 @@ def resta (num1,num2):
 def sumar(num1, num2):
     return num1 + num2
 
+def multiplicacion(num1, num2):
+    return num1*num2
