36af180586bb179db7c2941f3650526d15b96097
Ignacio Battaglino <battaglinoignacio@gmail.com>
2025-09-01 17:42:16 -0300

    Funcionalidad resta

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,14 @@
 
 def calcular (num1, num2, operacion):
-    if operacion == '+':
+    if operacion == '-':
+        return resta(num1, num2)
+    elif operacion == '+':
         return sumar(num1, num2)
+    return none
+
+def resta (num1,num2):
+    return num1-num2
     
 def sumar(num1, num2):
     return num1 + num2
+
