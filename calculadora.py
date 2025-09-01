7a8beff2446c5053696cfef33cf8f1d755cf6742
Pedro Carballo <carballopedro@hotmail.com>
2025-09-01 17:56:00 -0300

    Feature/division

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,3 @@
-
 def calcular (num1, num2, operacion):
     if (operacion == '*'):
         return multiplicacion(num1,num2)
@@ -6,7 +5,9 @@ def calcular (num1, num2, operacion):
         return resta(num1, num2)
     elif operacion == '+':
         return sumar(num1, num2)
-    return none
+    elif operacion == '/':
+        return division(num1, num2)
+    return 0
 
 def resta (num1,num2):
     return num1-num2
@@ -16,3 +17,8 @@ def sumar(num1, num2):
 
 def multiplicacion(num1, num2):
     return num1*num2
+
+def division (num1,num2):
+    if num2 == 0:
+        raise ValueError("No se puede dividir por cero")
+    return num1/num2
