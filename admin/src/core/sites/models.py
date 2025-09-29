996a8fb756672d6001f5fb833fee0cfeca68f1de
FedericoK <kesselmanfederico@gmail.com>
2025-09-29 13:44:10 -0300

    Completo models

--- admin/src/core/sites/models.py
+++ admin/src/core/sites/models.py
@@ -1,9 +1,23 @@
 from src.core.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import DateTime, String, UniqueConstraint
+from sqlalchemy import DateTime, String, UniqueConstraint, Table, Column, Integer, ForeignKey
 from datetime import datetime, timezone
 from geoalchemy2.types import Geometry
 from geoalchemy2.shape import to_shape
+from enum import Enum
+from sqlalchemy import Enum as SQLAlchemyEnum
+from sqlalchemy.orm import relationship
+
+class ConservationStatus(str, Enum):
+    GOOD = "Bueno"
+    REGULAR = "Regular" 
+    BAD = "Malo"
+
+class SiteCategory(str, Enum):
+    ARCHITECTURE = "Arquitectura"
+    INFRASTRUCTURE = "Infraestructura"
+    ARCHAEOLOGICAL = "Sitio arqueológico"
+    OTRO = "Otro"
 
 class Historic_Site(Base):
     """Represents an historic site"""
@@ -13,22 +27,48 @@ class Historic_Site(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
-    description: Mapped[str] = mapped_column(String(255), nullable=True)
+    short_description: Mapped[str] = mapped_column(String(255), nullable=False)
+    full_description: Mapped[str] = mapped_column(String(2000), nullable=False)
     city: Mapped[str] = mapped_column(String(120), nullable=False)
     province: Mapped[str] = mapped_column(String(120), nullable=False)
     location: Mapped[str] = mapped_column(Geometry(geometry_type='POINT', srid=4326), nullable=False)
-    # estado de conservacion
+    conservation_status: Mapped[ConservationStatus] = mapped_column(
+        SQLAlchemyEnum(ConservationStatus), nullable=False
+    )
     year: Mapped[int] = mapped_column(nullable=True)
-    # categoria
+    category: Mapped[SiteCategory] = mapped_column(
+        SQLAlchemyEnum(SiteCategory), nullable=False
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False
     )
+    is_visible: Mapped[bool] = mapped_column(default=False, nullable=False)
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=lambda: datetime.now(timezone.utc),
         onupdate=lambda: datetime.now(timezone.utc),
         nullable=False,
     )
+    tags = relationship("SiteTag", secondary=site_tag_association, back_populates="sites") # type: ignore
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "short_description": self.short_description,
+            "full_description": self.full_description,
+            "city": self.city,
+            "province": self.province,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "conservation_status": self.conservation_status,
+            "inauguration_year": self.inauguration_year,
+            "category": self.category,
+            "is_visible": self.is_visible,
+            "tags": [tag.name for tag in self.tags],
+            "created_at": self.created_at.isoformat() if self.created_at else None,
+            "updated_at": self.updated_at.isoformat() if self.updated_at else None,
+        }
 
 # Inversion entre latitud longitud con postgis
 @property
@@ -47,17 +87,20 @@ def longitude(self) -> float:
         return point.x # longitud
     return None
 
-# COMPLETAR con campos que faltan
-def to_dict(self) -> dict:
-    return {
-        "id": self.id,
-        "name": self.name,
-        "description": self.description,
-        "city": self.city,
-        "province": self.province,
-        "latitude": self.latitude,
-        "longitude": self.longitude,
-        "year": self.year,
-        "created_at": self.created_at.isoformat() if self.created_at else None,
-        "updated_at": self.updated_at.isoformat() if self.updated_at else None,
-    }
+# Tabla relacion entre sitios y etiquetas
+site_tag_association = Table(
+    'site_tag_association',
+    Base.metadata,
+    Column('site_id', Integer, ForeignKey('historic_sites.id'), primary_key=True),
+    Column('tag_id', Integer, ForeignKey('site_tags.id'), primary_key=True)
+)
+
+# Modelo para etiquetas
+class SiteTag(Base):
+    __tablename__ = 'site_tags'
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    
+    # Relación con sitios
+    sites = relationship("Historic_Site", secondary=site_tag_association, back_populates="tags")
