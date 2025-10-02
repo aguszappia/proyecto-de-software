fd5f64f26561f2d84c75b65b01c88164cf8e26dd
FedericoK <kesselmanfederico@gmail.com>
2025-10-02 12:52:58 -0300

    Arreglo imports de batabase

--- admin/src/web/controllers/__init__.py
+++ admin/src/web/controllers/__init__.py
@@ -4,14 +4,14 @@ from __future__ import annotations
 
 from flask import Flask
 
-from . import users, sites, tags
+from . import users, sites
 
 
 def register_controllers(app: Flask) -> None:
     """Register application blueprints."""
     app.register_blueprint(users.bp)
     app.register_blueprint(sites.bp)
-    app.register_blueprint(tags.bp)
+    # app.register_blueprint(tags.bp)
 
 
 __all__ = ["register_controllers"]
